9493744781c27ac59b7e2f22ed3ae0d71aef6782
MRYoung <mryoungforever33@gmail.com>
2015-06-12 14:40:19 +0800

    Create WhileLoop.c

---
+++ WhileLoop.c
@@ -0,0 +1,16 @@
+int main()
+{
+    int32_t i = 0;
+    while (i <= 100) {
+        if (i % 2 == 1) {
+        printf("The num is %d\n", i);
+        }
+        ++i;
+    }
+    
+    do{
+        printf("The num is %d\n", i);
+        ++i;
+    }while (i <= 100);
+    return 0;
+}
