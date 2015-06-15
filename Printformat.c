f8a74a693b3ba75f4aa475ebc5784983c103b403
MRYoung <mryoungforever33@gmail.com>
2015-06-15 19:32:18 +0800

    Create Printformat.c

---
+++ Printformat.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World\n");
+    printf("Number: %d\n", -3);
+    printf("Unsiged number: %u\n", 23576);
+    printf("%o\n", 8);
+    printf("%X, %x, Red:%X\n", 13, 12, 0xFFFF0000);
+    printf("%.2f\n", 3.14);
+    printf("%c\n", 'B');
+    printf("%s\n", "Hello World");
+    printf("%04d\n", 300);
+    return 0;
+}
