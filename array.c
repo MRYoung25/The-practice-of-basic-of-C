f1e6baa838d24caf9a1bb2b4663dbb0814b9788a
MRYoung <mryoungforever33@gmail.com>
2015-06-17 00:04:31 +0800

    Update array.c

--- array.c
+++ array.c
@@ -16,5 +16,23 @@ int main()
     for (int i = 0; i < 6; i++) {
         printf("%d\n", Arr[i]);
     }
+    int ARR[3][4];
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            ARR[i][j] = 0;
+            printf("index(%d,%d)%d\n", i, j, ARR[i][j]);
+        }
+    }
+    printf("%d\n", ARR[2][1]);
+    
+    int arr1[3][4] = {{1, 2, 3, 4},
+                    {5, 6, 7, 8},
+                    {9, 10, 11, 12}
+                    };
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("index(%d,%d)%d\n", i, j, arr1[i][j]);
+        }
+    }
     return 0;
 }
