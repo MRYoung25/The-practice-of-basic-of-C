c0fefc338be5ec239cc72c13103b6b41a5ea4f03
MRYoung <mryoungforever33@gmail.com>
2015-06-18 09:37:47 +0800

    Create Functions.c

---
+++ Functions.c
@@ -0,0 +1,25 @@
+#include <iostream>
+
+void printHelloWorld(){
+    printf("Hello World\n");
+}
+
+void Hello(char * name, int age){
+    printf("Hello %s, You age is %d\n", name, age);
+}
+
+int add(int a, int b){
+    return a + b;
+}
+
+int main()
+{
+    printHelloWorld();
+    
+    Hello("Young",33);
+    
+    int c = add(1,2);
+    printf("The result is %d\n", c);
+    
+    return 0;
+}
